79d88fc70fc90581131b2f14f3e1d67a1f009d64
Gerardo Belot <gbelot2003@hotmail.com>
2024-10-01 14:08:05 -0600

    :construction: Configuraciones

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# app.py
+
 from flask import Flask, render_template, request, jsonify
 from src.routes.router import configure_routes
 app = Flask(__name__)
--- src/routes/twilio.py
+++ src/routes/twilio.py
@@ -3,15 +3,19 @@ from flask import request, jsonify
 from src.services.openai_service import OpenAIService
 
 def configure_twilio_routes(app):
-    # Ruta para el webhook
     @app.route('/api/twilio', methods=['POST'])
     def twilio_webhook():
-        
-        # Obtener datos desde mensajes de API
-        message_body = request.form.get('Body')
-        from_number = request.form.get('From')
+        try:
+            # Obtener datos desde mensajes de API
+            message_body = request.form.get('Body')
+            from_number = request.form.get('From')
 
-        response = OpenAIService().handle_request(message_body)
+            #if not message_body or not from_number:
+            #    return jsonify({"error": "Missing required parameters"}), 400
 
-         # Devolver la respuesta a Twilio
-        return jsonify({"message": response})
+            response = OpenAIService().handle_request(message_body)
+
+            # Devolver la respuesta a Twilio
+            return jsonify({"message": response})
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
